78958d613594aef8db8f4222918d0af7a0644f1a
Gwangho Kim <gwangho.kim@sima.ai>
2023-07-26 11:47:05 -0700

    resize option added

--- test_model.py
+++ test_model.py
@@ -3,13 +3,14 @@ import torch.nn.functional as F
 import numpy as np
 import cv2
 from imread_from_url import imread_from_url
+from nets.utils.utils import dxs,dys
 
 from nets import Model
 
 device = 'cuda'
 
 #Ref: https://github.com/megvii-research/CREStereo/blob/master/test.py
-def inference(left, right, model, n_iter=20):
+def inference(left, right, model, size, n_iter=20):
 
     print("Model Forwarding...")
     imgL = left.transpose(2, 0, 1)
@@ -22,13 +23,13 @@ def inference(left, right, model, n_iter=20):
 
     imgL_dw2 = F.interpolate(
         imgL,
-        size=(imgL.shape[2] // 2, imgL.shape[3] // 2),
+        size=size,
         mode="bilinear",
         align_corners=True,
     )
     imgR_dw2 = F.interpolate(
         imgR,
-        size=(imgL.shape[2] // 2, imgL.shape[3] // 2),
+        size=size,
         mode="bilinear",
         align_corners=True,
     )
@@ -57,13 +58,35 @@ if __name__ == '__main__':
     imgR = cv2.resize(right_img, (eval_w, eval_h), interpolation=cv2.INTER_LINEAR)
 
     model_path = "models/crestereo_eth3d.pth"
+    print(dxs,dys)
 
     model = Model(max_disp=256, mixed_precision=False, test_mode=True)
     model.load_state_dict(torch.load(model_path), strict=True)
     model.to(device)
     model.eval()
 
-    pred = inference(imgL, imgR, model, n_iter=20)
+    scale_factor,downsample = 1,True    # max(dx),max(dy)=21,4
+    scale_factor,downsample = 2,True    # max(dx),max(dy)=22,4
+    scale_factor,downsample = 4,True    # max(dx),max(dy)=27,4
+    scale_factor,downsample = 2,False   # max(dx),max(dy)=32,4
+    scale_factor,downsample = 4,False   # max(dx),max(dy)=46,5
+
+    assert imgL.shape == imgR.shape
+    H,W,_ = imgL.shape
+    def resize(scale_factor, downsample): # {
+        assert scale_factor in [1,2,4]
+        if downsample:
+            H_, W_ = H // scale_factor, W // scale_factor
+        else:
+            H_, W_ = H * scale_factor, W * scale_factor
+        print(f'H*W={H*W}')
+        print(f'H_*W_={H_*W_}')
+        return (H_, W_)
+    # }; end of resize
+    size = (H_,W_) = resize(scale_factor, downsample)
+    pred = inference(imgL, imgR, model, size, n_iter=20)
+#   print(dxs, dys)
+    print(max(dxs), max(dys))
 
     t = float(in_w) / float(eval_w)
     disp = cv2.resize(pred, (in_w, in_h), interpolation=cv2.INTER_LINEAR) * t
@@ -73,7 +96,12 @@ if __name__ == '__main__':
     disp_vis = cv2.applyColorMap(disp_vis, cv2.COLORMAP_INFERNO)
 
     combined_img = np.hstack((left_img, disp_vis))
-    cv2.namedWindow("output", cv2.WINDOW_NORMAL)
-    cv2.imshow("output", combined_img)
-    cv2.imwrite("output.jpg", disp_vis)
+#   cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+#   cv2.imshow("output", combined_img)
+    _ = '_' if downsample else ''
+    output_file = f'output{H}x{W}-{H_}x{W_}.jpg'
+    cv2.imwrite(output_file, disp_vis)
     cv2.waitKey(0)
+
+
+
